67a308447c3dd3e1d6209d4cd61d0c52007a0f52
Erick Cezar <80288259+erick-cezar@users.noreply.github.com>
2022-04-18 20:49:03 -0300

    Create configurações do firebase.js
    Ajustando o firebase e o react para dar inicio ao projeto de clonagem.

---
+++ configurações do firebase.js
@@ -0,0 +1,25 @@
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOK7x5N5UnjY4TDqndzH7l5tvdNIsWFRc",
+  authDomain: "todo-app-e3cf0.firebaseapp.com",
+  projectId: "todo-app-e3cf0",
+  storageBucket: "todo-app-e3cf0.appspot.com",
+  messagingSenderId: "940016886081",
+  appId: "1:940016886081:web:91686613f16b1b1f8001c0",
+  measurementId: "G-JHPC7TP12K"
+};
+
+// Use this to initialize the firebase App
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// Use these for db & auth
+const db = firebaseApp.firestore();//database
+const auth = firebase.auth(); //login
+const storage = firebase.storage();//upload
+const functions = firebase.functions();
+
+export { auth, db };
